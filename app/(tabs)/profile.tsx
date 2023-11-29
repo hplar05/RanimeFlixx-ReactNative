ed535777a0b5900b564fcdea3da40be0e9c58b45
Ralph Saladino <130468937+hplar05@users.noreply.github.com>
2023-11-29 23:22:42 +0800

    feat: sign out

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,10 +1,20 @@
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
 import React from "react";
+import { useAuth } from "@clerk/clerk-expo";
+import { Link, useRouter } from "expo-router";
 
 const profile = () => {
+  const { signOut, isSignedIn } = useAuth();
+  const router = useRouter();
+
   return (
     <View>
-      <Text>profile</Text>
+      <Button title="Log out" onPress={() => signOut()} />
+      {!isSignedIn && (
+        <Link href={"/(modals)/login"}>
+          <Text>Login</Text>
+        </Link>
+      )}
     </View>
   );
 };
