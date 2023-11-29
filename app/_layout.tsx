8d4853a78fbe41f363848022db9865ed146c17a1
Ralph Saladino <130468937+hplar05@users.noreply.github.com>
2023-11-29 19:18:45 +0800

    done: initial setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,8 @@
+import { Ionicons } from "@expo/vector-icons";
 import { useFonts } from "expo-font";
-import { SplashScreen, Stack } from "expo-router";
+import { SplashScreen, Stack, useRouter } from "expo-router";
 import { useEffect } from "react";
+import { TouchableOpacity } from "react-native";
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -18,7 +20,7 @@ SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
   const [loaded, error] = useFonts({
     mon: require("../assets/fonts/Montserrat-Regular.ttf"),
-    "mon-sm": require("../assets/fonts/Montserrat-SemiBold.ttf"),
+    "mon-sb": require("../assets/fonts/Montserrat-SemiBold.ttf"),
     "mon-b": require("../assets/fonts/Montserrat-Bold.ttf"),
   });
 
@@ -41,14 +43,34 @@ export default function RootLayout() {
 }
 
 function RootLayoutNav() {
+  const router = useRouter();
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       <Stack.Screen
         name="(modals)/login"
         options={{
-          title: "Login",
+          title: "Login or Sign Up",
+          headerTitleStyle: {
+            fontFamily: "mon-sb",
+          },
           presentation: "modal",
+          headerTitleAlign: "center",
+          headerLeft: () => (
+            <TouchableOpacity
+              onPress={() => {
+                router.back();
+              }}
+            >
+              <Ionicons name="close-outline" size={30} />
+            </TouchableOpacity>
+          ),
+        }}
+      />
+      <Stack.Screen
+        name="animedetails/[id]"
+        options={{
+          headerTitle: "",
         }}
       />
     </Stack>
