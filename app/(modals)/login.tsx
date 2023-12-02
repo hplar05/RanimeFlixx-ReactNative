fbd0e3ae9c0e4ac4806d5d5be60fe9e52c03af2a
Ralph Saladino <130468937+hplar05@users.noreply.github.com>
2023-12-02 19:59:28 +0800

    done: login auth

--- app/(modals)/login.tsx
+++ app/(modals)/login.tsx
@@ -9,15 +9,57 @@ import {
   Ionicons,
   MaterialCommunityIcons,
 } from "@expo/vector-icons";
+import { useRouter } from "expo-router";
+import { useOAuth } from "@clerk/clerk-expo";
+
+enum Strategy {
+  Google = "oauth_google",
+  Facebook = "oauth_facebook",
+  Github = "oauth_github",
+  Tiktok = "oauth_tiktok",
+  Apple = "oauth_apple",
+}
 
 const login = () => {
   useWarmUpBrowser();
+  const router = useRouter();
+
+  const { startOAuthFlow: googleAuth } = useOAuth({ strategy: "oauth_google" });
+  const { startOAuthFlow: facebookAuth } = useOAuth({
+    strategy: "oauth_facebook",
+  });
+  const { startOAuthFlow: githubAuth } = useOAuth({ strategy: "oauth_github" });
+  const { startOAuthFlow: tiktokAuth } = useOAuth({ strategy: "oauth_tiktok" });
+  const { startOAuthFlow: appleAuth } = useOAuth({ strategy: "oauth_apple" });
+
+  const onSelectAuth = async (strategy: Strategy) => {
+    const selectedAuth = {
+      [Strategy.Google]: googleAuth,
+      [Strategy.Facebook]: facebookAuth,
+      [Strategy.Github]: githubAuth,
+      [Strategy.Tiktok]: tiktokAuth,
+      [Strategy.Apple]: appleAuth,
+    }[strategy];
+
+    try {
+      const { createdSessionId, setActive } = await selectedAuth();
+      console.log(createdSessionId);
+
+      if (createdSessionId) {
+        setActive!({ session: createdSessionId });
+      }
+    } catch (err) {
+      console.error(`OAtuh error: ${err}`);
+      console.log(`${err} Error`);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <TextInput
         autoCapitalize="none"
         placeholder="Email"
-        style={[defaultStyles.inputField, { marginBottom: 30 }]}
+        style={[defaultStyles.inputField, { marginBottom: 30, marginTop: 20 }]}
       />
       <TouchableOpacity style={defaultStyles.btn}>
         <Text style={defaultStyles.btnText}>Continue</Text>
@@ -42,36 +84,10 @@ const login = () => {
       </View>
 
       <View style={{ gap: 20 }}>
-        <TouchableOpacity style={styles.btnOutline}>
-          <Ionicons
-            name="call-outline"
-            style={defaultStyles.btnIcon}
-            size={24}
-          />
-          <Text style={styles.btnOutlineText}>Continue with Phone</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity style={styles.btnOutline}>
-          <Ionicons
-            name="md-logo-facebook"
-            style={defaultStyles.btnIcon}
-            size={24}
-          />
-          <Text style={styles.btnOutlineText}>Continue with Facebook</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.btnOutline}>
-          <FontAwesome5 name="tiktok" style={defaultStyles.btnIcon} size={24} />
-          <Text style={styles.btnOutlineText}>Continue with Tiktok</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.btnOutline}>
-          <Ionicons
-            name="md-logo-apple"
-            style={defaultStyles.btnIcon}
-            size={24}
-          />
-          <Text style={styles.btnOutlineText}>Continue with Apple</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.btnOutline}>
+        <TouchableOpacity
+          style={styles.btnOutline}
+          onPress={() => onSelectAuth(Strategy.Google)}
+        >
           <Ionicons
             name="md-logo-google"
             style={defaultStyles.btnIcon}
@@ -79,13 +95,45 @@ const login = () => {
           />
           <Text style={styles.btnOutlineText}>Continue with Google</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.btnOutline}>
+        <TouchableOpacity
+          style={styles.btnOutline}
+          onPress={() => onSelectAuth(Strategy.Facebook)}
+        >
+          <Ionicons
+            name="md-logo-facebook"
+            style={defaultStyles.btnIcon}
+            size={24}
+          />
+          <Text style={styles.btnOutlineText}>Continue with Facebook</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.btnOutline}
+          onPress={() => onSelectAuth(Strategy.Github)}
+        >
           <Ionicons
             name="md-logo-github"
             style={defaultStyles.btnIcon}
             size={24}
           />
           <Text style={styles.btnOutlineText}>Continue with Github</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.btnOutline}
+          onPress={() => onSelectAuth(Strategy.Tiktok)}
+        >
+          <FontAwesome5 name="tiktok" style={defaultStyles.btnIcon} size={24} />
+          <Text style={styles.btnOutlineText}>Continue with Tiktok</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.btnOutline}
+          onPress={() => onSelectAuth(Strategy.Apple)}
+        >
+          <Ionicons
+            name="md-logo-apple"
+            style={defaultStyles.btnIcon}
+            size={24}
+          />
+          <Text style={styles.btnOutlineText}>Continue with Apple</Text>
         </TouchableOpacity>
       </View>
     </View>
